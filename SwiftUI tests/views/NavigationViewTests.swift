fd7421085c0afd4831c68ce6ff58670c356d34a7
Federico López <gatolab@gmail.com>
2020-10-01 13:30:08 -0300

    More tests with NavigationView

--- SwiftUI tests/views/NavigationViewTests.swift
+++ SwiftUI tests/views/NavigationViewTests.swift
@@ -9,12 +9,66 @@ import SwiftUI
 
 // https://www.hackingwithswift.com/articles/216/complete-guide-to-navigationview-in-swiftui
 struct NavigationViewTests: View {
+
+    @State private var goTo: String?
+
+    init() {
+        let myCfg = UINavigationBarAppearance()
+        myCfg.configureWithOpaqueBackground()
+        myCfg.backgroundColor = .green
+        myCfg.titleTextAttributes = [.foregroundColor: UIColor.white]
+        myCfg.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().standardAppearance = myCfg
+        UINavigationBar.appearance().scrollEdgeAppearance = myCfg
+    }
+
+
     var body: some View {
         
+        /*
         NavigationView {
             ColoredView(color: .constant(.green),
                         title: .constant("Green"))
         }
+        */
+        
+        /*
+        NavigationView {
+        
+            VStack {
+                Text("asdasdasd")
+                    .navigationBarTitle("hola")
+                NavigationLink(
+                    destination: Text("Destination")
+                        .navigationBarTitle("chau")
+                    ,tag: "next",
+                    selection: $goTo,
+                    label: {Text("Navigate")}
+                )
+            }
+        }
+        */
+        
+        NavigationView {
+            
+            VStack {
+                NavigationLink(
+                    destination: Text("Info view!"),
+                    tag: "info",
+                    selection: $goTo,
+                    label: { EmptyView() }
+                )
+                
+                Button(action: {
+                    self.goTo = "info"
+                }, label: {
+                    Text("Button")
+                })
+            }
+            
+        }
+        
         
     }
 }
